b5ae9e03bcafb488e4cd9861f8b6d2c54dc4731c
tenzijn <tenzinkunchok2022@gmail.com>
2023-12-08 12:37:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# App Starter
+
+Here I have included list of starter projects for different frameworks and libraries.
+
+### WebPack
+
+- branch: webpack-v1
+
+### Vite
+
+- branch: vite-v1
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: {
+      import: './src/script/index.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Tenzin`s Webpack Starter',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+};
